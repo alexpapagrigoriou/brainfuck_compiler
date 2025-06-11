8ee8c4e61966569e7da86830710a9ea2081c3030
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2025-06-12 02:17:11 +0300

    Commented unused unused functions.

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 int main(int argc, char *argv[]) {
     check_args(argc, argv);
 
-    char *input_file, *output_file;
-    find_input_output_files(argc, argv, input_file, output_file);
+    // char *input_file, *output_file;
+    // find_input_output_files(argc, argv, input_file, output_file);
 
-    open_input_file(input_file);
+    // open_input_file(input_file);
 
-    check_if_input_is_valid();
+    // check_if_input_is_valid();
 
-    create_output_file();
+    // create_output_file();
 
     return 0;
 }
