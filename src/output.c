51bdc1b1bbb15c6daa14598304de9ef09a8f4992
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2025-06-12 05:24:00 +0300

    Updated ouput (not finished).

--- src/output.c
+++ src/output.c
@@ -1,5 +1,15 @@
 #include "output.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void create_output_file(const char *code, int comma_counter, const char *output_file) {
+    FILE *file = fopen(output_file, "wx");
+    if (file == NULL) {
+        perror("Error opening file.\n");
+        exit(EXIT_FAILURE);
+    }
 
+    fprintf(file, "");
+
+    fclose(file);
 }
