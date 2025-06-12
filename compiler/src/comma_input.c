693d27a87a3fef65c12183cc9703e163d2bd9efa
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2025-06-12 05:41:41 +0300

    Added compiler files.

---
+++ compiler/src/comma_input.c
@@ -0,0 +1,48 @@
+#include "comma_input.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char *comma_inputs;
+static size_t comma_index = 0;
+
+void get_comma_inputs(int comma_counter) {
+    if (comma_counter <= 0) {
+        comma_inputs = NULL;
+        return;
+    }
+
+    comma_inputs = malloc(comma_counter);
+    if (comma_inputs == NULL) {
+        perror("Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Comma inputs (Provide %d character%s for ',' commands):\n", comma_counter, (comma_counter == 1 ? "" : "s"));
+
+    size_t comma_index = 0;
+    char line[1024];
+
+    while (comma_index < (size_t) comma_counter) {
+        if (fgets(line, sizeof(line), stdin) == NULL) {
+            fprintf(stderr, "Error reading input.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        for (size_t i = 0; i < strlen(line) && comma_index < (size_t) comma_counter; i++) {
+            if (line[i] != '\n') {
+                comma_inputs[comma_index++] = line[i];
+            }
+        }
+
+        if (comma_index < (size_t) comma_counter) {
+            int commas_left = comma_counter - (int) comma_index;
+            printf("\nComma inputs (Provide %d more character%s for ',' commands):\n", commas_left, (commas_left == 1 ? "" : "s"));
+        }
+    }
+    printf("\n\n");
+}
+
+char next_comma_input() {
+    return comma_inputs[comma_index++];
+}
---
+++ compiler/src/comma_input.h
@@ -0,0 +1,7 @@
+#ifndef COMMA_INPUT_H
+#define COMMA_INPUT_H
+
+void get_comma_inputs(int comma_counter);
+char next_comma_input();
+
+#endif
